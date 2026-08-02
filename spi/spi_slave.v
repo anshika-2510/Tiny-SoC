e865363655f882a5d956cd47859bed439d194a25
Anshika Gupta <ag.anshika25@gmail.com>
2026-08-02 17:58:09 +0530

    Refactor SPI slave module for better signal handling
    Refactor SPI slave module to improve signal handling and reset logic.

--- spi/spi_slave.v
+++ spi/spi_slave.v
@@ -1,4 +1,3 @@
-
 module spi_slave(input sclk,
 input cs,
 input mosi,
@@ -14,39 +13,41 @@ reg [2:0] ct;
 
 always @(negedge cs)
 begin
-tx_shift<=tx_data;
-//miso <= tx_data[7];
-ct<=0;
-rx_shift<=0;
-rx_data<=0;
+    tx_shift <= tx_data << 1;
+    miso     <= tx_data[7];
+    ct       <= 0;
+    rx_shift <= 0;
+    rx_data  <= 0;
 end
-always @ (posedge sclk or posedge rst)
+//falling edge--> transmit next bit
+always @(negedge sclk or posedge rst)
 begin
- if(rst)
+    if (rst)
+    begin
+        miso <= 0;
+        tx_shift <= 0;
+    end
+    else if (!cs)
+    begin
+        miso <= tx_shift[7];
+        tx_shift <= tx_shift << 1;
+    end
+end
+
+//rising edge---> sample current bit
+always @(posedge sclk or posedge rst)
+begin
+    if (rst)
     begin
         ct <= 0;
-        tx_shift <= 0;
         rx_shift <= 0;
         rx_data <= 0;
-        miso <= 0;
     end
- else
- begin
- 
- 
-if (cs==0)
-begin
-miso <= tx_shift[7];
-tx_shift <= tx_shift << 1;
- rx_shift <= {rx_shift[6:0], mosi};
-if(ct == 6)
-begin
-   rx_data <= {rx_shift[6:0], mosi};
-    ct <= ct + 1;
+    else if (!cs)
+    begin
+        rx_shift <= {rx_shift[6:0], mosi};
+        rx_data  <= {rx_shift[6:0], mosi};
+        ct <= ct + 1;
+    end
 end
-else
-    ct <= ct + 1;
-  end 
-end
-end    
 endmodule
